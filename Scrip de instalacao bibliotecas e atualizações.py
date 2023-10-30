79d6179280590b498095c35456d68af84a314b8f
JOHANES IVAN MULLER <johanesmm@gmail.com>
2023-10-30 10:40:15 -0300

    Add files via upload

---
+++ Scrip de instalacao bibliotecas e atualizações.py
@@ -0,0 +1,32 @@
+# Instalar a versão completa do python, marcar todas as oções https://www.python.org/downloads/windows/
+# Instalar o VS Code https://code.visualstudio.com/Download
+# No VS Code View > extensions ou CTRL + SHIFT + X > python
+# duplicar linha no VS = SHIFT + ALT + DownArrow
+# mover linha no VS  =  ALT + Setas
+# CTRL + ALT aumenta o tamannho do cursor, serve para comendar uma parte maior do codigo
+
+
+# No Sicredi
+#pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org black
+
+import os
+
+#os.system('python.exe -m pip install --upgrade pip')
+
+#os.system('pip install pandas')
+#os.system('pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org pandas')
+
+#os.system('pip install openpyxl')
+#os.system('pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org openpyxl')
+
+#os.system('pip install jupyter')
+#os.system('pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org jupyter')
+
+#os.system('pip install selenium')
+#os.system('pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhoste.org selenium') 
+
+# verificar versões 
+#os.system('python --version ')
+#os.system('pip --version ')
+
+
